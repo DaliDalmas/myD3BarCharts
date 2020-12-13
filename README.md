c1dab0de02ed089d380fa2af77d64dfe850f5f49
apple <tutor.dalmas.otieno@gmail.com>
2020-12-13 16:47:24 +0300

    sorting the data

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # myD3BarCharts
 I have gone through an introduction to data visualisation using D3.js and today I learnt how to plot bar charts. This repository contains my personal practise code.
 
-The data from these charts is gotten from Kaggle (https://www.kaggle.com/babyoda/women-entrepreneurship-and-labor-force)
+The Women Entrepreneurship and Labor Force data from these charts is gotten from Kaggle (https://www.kaggle.com/babyoda/women-entrepreneurship-and-labor-force)
--- index.js
+++ index.js
@@ -6,8 +6,50 @@ svg.style("background-color", `${canvasColor}`);
 const width = +svg.attr('width')
 const height = +svg.attr('height')
 
-d3.csv('https://raw.githubusercontent.com/DaliDalmas/myD3BarCharts/main/Dataset3.csv').then(
+
+
+
+const render = data => {
+    const yValue = d=>d["Country"]
+    const xValue = d=>d["Female Labor Force Participation Rate"]
+    const margin = {top:20,bottom:20,right:40,left:200}
+    const innerWidth = width-margin.left-margin.right;
+    const innerHeight = height-margin.top-margin.bottom;
+
+
+    const xScale = d3.scaleLinear()
+                    .domain([0,d3.max(data,xValue)])
+                    .range([0,innerWidth]);
+
+    const yScale = d3.scaleBand()
+                    .domain(data.map(yValue))
+                    .range([0,innerHeight])
+                    .padding(0.1);
+
+    const g = svg.append('g')
+                .attr('transform',`translate(${margin.left},${margin.top})`);
+
+    g.append('g').call(d3.axisLeft(yScale));
+
+    g.append('g').call(d3.axisBottom(xScale))
+        .attr('transform',`translate(0,${innerHeight})`);;
+
+    g.selectAll('rect')
+    .data(data)
+    .enter()
+    .append('rect')
+    .attr('y',d=>yScale(yValue(d)))
+    .attr('width',d=>xScale(xValue(d)))
+    .attr('height',yScale.bandwidth())
+};
+
+d3.csv('https://raw.githubusercontent.com/DaliDalmas/myD3BarCharts/main/Women%20Entrepreneurship%20and%20Labor%20Force.csv').then(
     data=>{
+        data.forEach(d => {
+            d['Female Labor Force Participation Rate']=+d['Female Labor Force Participation Rate']
+        });
+
+        render(data)
         console.log(data)
     }
 )
